db236ad2225d2b76b0ec157a9a356e252f1eb61c
Ivan Vidic <ividic010@gmail.com>
2022-06-10 11:05:19 +0200

    simplified image change

--- main.js
+++ main.js
@@ -14,25 +14,15 @@ images[2] = "pexels-jessica-lewis-creative-593322.jpg";
 images[3] = "pexels-quintin-gellar-313782.jpg";
 images[4] = "pexels-s-migaj-747964.jpg";
 
-// Change Image
-function changeImg(){
-	document.slide.src = images[i];
 
-	// Check If Index Is Under Max
-	if(i < images.length - 1){
-	  // Add 1 to Index
-	  i++; 
-	} else { 
-		// Reset Back To O
-		i = 0;
-	}
 
-	// Run function every x seconds
-	setTimeout("changeImg()", time);
-}
 
-// Run function when page loads
-window.onload=changeImg;
+setInterval(() => {
+  document.slide.src = images[i % images.length];
+  i++;
+}, time);
+
+
 
 
 
